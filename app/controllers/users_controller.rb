4fff0b67db077aa182bb7287769eee65c537fbf7
ccarruitero <cesar@mozilla.pe>
2015-01-20 12:15:38 -0500

    set edit and update for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
+  before_filter :set_user
+
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render action: 'edit'
+    end
+  end
+
+  private
+
+  def set_user
+    @user = @current_user || User.find(params[:id])
+  end
+
+  def user_paramas
+    params.require(:user).permit(:twitter_id)
   end
 end
