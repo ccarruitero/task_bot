5c75c276e54aa6b8501560851e45a67239b6fc89
ccarruitero <cesar@mozilla.pe>
2015-01-20 11:49:18 -0500

    add missing migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150120155048) do
+ActiveRecord::Schema.define(version: 20150120161403) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,8 +20,10 @@ ActiveRecord::Schema.define(version: 20150120155048) do
     t.string   "name"
     t.string   "email"
     t.string   "twitter_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.string   "token"
+    t.string   "refresh_token"
   end
 
 end
