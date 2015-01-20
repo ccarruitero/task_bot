df3381952d4680202700be14d0d55d9f28cdacf9
ccarruitero <cesar@mozilla.pe>
2015-01-20 11:49:04 -0500

    add controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,20 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :login_required
+  helper_method :current_user
+
+  def current_user
+    @current_user ||= User.find_by(token: session[:access_token]) if session[:access_token]
+  end
+
+  private
+
+  def login_required
+    !current_user.nil? || access_denied
+  end
+
+  def access_denied
+    redirect_to login_url
+  end
 end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  skip_before_filter :login_required
+
+  def new
+  end
+
+  def create
+    auth = request.env['omniauth.auth']
+    user = User.find_or_create_by(email: auth['info']['email'])
+    user.update(token: auth['credentials']['token'],
+                refresh_token: auth['credentials']['refresh_token'])
+    session[:access_token] = user.token
+    redirect_to root_url, notice: 'You are logged in!'
+  end
+
+  def destroy
+    session[:access_token] = nil
+    redirect_to root_url, notice: 'Logged out!'
+  end
+end
